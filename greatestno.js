ac832b81938b93e098015b8665538fdedf227fba
shaquibkhan <connectshaquib@gmail.com>
2017-04-29 16:53:48 +0530

    commit
    commit

---
+++ greatestno.js
@@ -0,0 +1,16 @@
+// three variables declared here ...
+var num1 =590;
+var num2 = 1000;
+var num3 =3030;
+// nested if else used here ....
+if (num1>num2 && num1>num3) {
+	console.log(num1+ " IS GREATEST AMONG "+num1+ " " +num2+ " " +num3);
+}
+ else if(num2>num1 && num2>num3)
+{
+	console.log(num2+ " IS GREATEST AMONG "+num1+ " " +num2+ " " +num3);
+}
+else
+{
+	console.log(num3+ " IS GREATEST AMONG "+num1+ " " +num2+ " " +num3);	
+}
